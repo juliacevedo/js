68de0c0ece9bc3bbe197f430403f41b1fa905483
Julia Acevedo Fuentes <juliacevedo0103gmail.com>
2023-03-28 16:39:10 -0300

    Header adittion

--- js/inicio.js
+++ js/inicio.js
@@ -15,23 +15,36 @@ const producto = a * b
 const promedio = ( a + b ) / 2
 const porcentaje = Promedio * 100
 
-//Calculator?
-let screen = document.getElementById('screen');
-const buttons = document.querySelectorAll("#buttons a");
- 
-for (const button of buttons) {
-    button.addEventListener('click', function(e) {
-        e.preventDefault();
- 
-        if (e.target.dataset.key == 'equal') {
-            screen.textContent = eval(screen.textContent);
-            if (screen.textContent.length > 8) {
-                screen.textContent = eval(screen.textContent).toFixed(8);
-            }
-        } else if (e.target.dataset.key == 'clear') {
-            screen.textContent = '';
-        } else {
-            screen.textContent = screen.textContent + e.target.dataset.key;
-        }   
-    });
+//Button with reaction + 1st event // vamos a usar esto para hacer que vea si el numero que sale en calculadora es par o no, al principio va a pedir un numero al usuario y le va a dar resultados nomas
+function UGH () {
+    let parrafo = document.getElementById ("parrafo")
+    parrafo.innerHTML = "<h2>UGH</h2>"
+    document.body.appendChild(parrafo)
+    parrafo.addEventListener("click", Respuestaclick)    
 }
+
+UGH ()
+
+function Respuestaclick (){  
+    parrafo.innerHTML = "<h3>Thank You...</h3>" 
+    parrafo.addEventListener("click", Respuestaclick2)
+}
+
+function Respuestaclick2 (){  
+    parrafo.innerHTML = "<h3>You are very welcome!</h3>" 
+    parrafo.addEventListener("click", Respuestaclick3)
+}
+
+function Respuestaclick3() {
+    Swal.fire('Very welcome!!!!:))')
+}
+
+// program to check if the number is even or odd
+// take input from the user
+const number = prompt("Enter a number: ");
+
+// ternary operator
+const result = (number % 2  == 0) ? "even" : "odd";
+
+// display the result
+console.log(`The number is ${result}.`);
